fd0e6fe2adc831b542503513243aed177e2da7e4
Youngho Jo <dodghek1@naver.com>
2018-03-23 15:49:20 +0900

    start Gradle project

---
+++ src/main/java/com/openhome/OpenHome/OpenHomeApplication.java
@@ -0,0 +1,12 @@
+package com.openhome.OpenHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenHomeApplication.class, args);
+	}
+}
