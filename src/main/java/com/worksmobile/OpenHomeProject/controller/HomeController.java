b2f175bef31e7d98b07f11bb6a12845567abc174
Youngho Jo <dodghek1@naver.com>
2018-03-27 19:00:05 +0900

    Port is changed from 9000 to 8080(default port)

--- src/main/java/com/worksmobile/OpenHomeProject/controller/HomeController.java
+++ src/main/java/com/worksmobile/OpenHomeProject/controller/HomeController.java
@@ -3,9 +3,7 @@ package com.worksmobile.OpenHomeProject.controller;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.worksmobile.OpenHomeProject.service.BoardService;
@@ -18,9 +16,6 @@ public class HomeController {
 	
 	@RequestMapping(value="/index")
 	public String index() {
-		/*ModelAndView mav = new ModelAndView();
-		mav.setViewName("hello");
-		return mav;*/
 		return "layout";
 	}
 	
--- src/main/java/com/worksmobile/OpenHomeProject/dao/BoardDAO.java
+++ src/main/java/com/worksmobile/OpenHomeProject/dao/BoardDAO.java
@@ -1,9 +1,6 @@
 package com.worksmobile.OpenHomeProject.dao;
 
 import java.util.List;
-
-import org.springframework.stereotype.Repository;
-
 import com.worksmobile.OpenHomeProject.dto.BoardDTO;
 
 public interface BoardDAO {
