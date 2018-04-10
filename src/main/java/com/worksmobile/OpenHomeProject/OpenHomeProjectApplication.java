c09c9cc0ea80464b8963e78052cc853e28b4abc5
Youngho Jo <dodghek1@naver.com>
2018-04-10 21:47:34 +0900

    branch test1

--- src/main/java/com/worksmobile/OpenHomeProject/OpenHomeProjectApplication.java
+++ src/main/java/com/worksmobile/OpenHomeProject/OpenHomeProjectApplication.java
@@ -9,7 +9,6 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.support.ErrorPageFilter;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
